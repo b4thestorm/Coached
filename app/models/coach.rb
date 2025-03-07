324749ee2e8ce4aab2455b5056ccdc916213a632
arnoldsanders <arnoldsander@gmail.com>
2025-03-07 04:24:09 -0500

    model views

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -4,5 +4,15 @@ class Coach < ApplicationRecord
 
     validates :name, presence: true
 
-    # scope :avilability, -> {}
+    def upcoming_schedule
+        #NOTE:I know i shouldnt reference another class here, but it's simpler than the associations
+        #SELECT "schedules".* FROM "schedules" WHERE (coach_id = 3 AND slot_start > '2025-03-07 09:11:06.869444') /* loading for pp */ LIMIT $1  [["LIMIT", 11]] #Noooice
+        Schedule.where("coach_id = ? AND slot_start > ?", self.id, DateTime.now)
+    end
+
+    def self.general_coach_availability
+        all.each do | coach |
+            coach.upcoming_schedule
+        end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
     has_many :schedules
     has_many :coaches, through: :schedules
+
+
 end
